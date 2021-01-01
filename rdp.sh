71beab4572018e4ab77b4f63777df97791400f33
crypt-bphc <76141508+crypt-bphc@users.noreply.github.com>
2021-01-01 20:18:20 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,24 +1,7 @@
 #! /bin/bash
 printf "Installing RDP Be Patient... " >&2
 {
-sudo useradd -m Arihant
-sudo adduser Arihant sudo
-echo 'password' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt-get update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo adduser Arihant chrome-remote-desktop
+sudo apt update
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
