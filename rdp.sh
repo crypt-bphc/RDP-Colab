e95ad973512664b3135affcfa16e9756b0453a89
crypt-bphc <76141508+crypt-bphc@users.noreply.github.com>
2021-01-01 20:26:01 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -5,10 +5,10 @@ sudo apt update
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+printf '\nCheck https://remotedesktop.google.com/u/3/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
 su - Arihant -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
+printf 'Check https://remotedesktop.google.com/u/3/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
     printf "\n\nUpgrade Completed " >&2
